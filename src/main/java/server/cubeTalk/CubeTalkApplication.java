460c803f1ce0c0e30a63075504ab5869dedc2501
leedabin2 <12193152@inha.edu>
2024-08-22 16:29:39 +0900

    frist commit

---
+++ src/main/java/server/cubeTalk/CubeTalkApplication.java
@@ -0,0 +1,13 @@
+package server.cubeTalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CubeTalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CubeTalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/server/cubeTalk/CubeTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package server.cubeTalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CubeTalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
