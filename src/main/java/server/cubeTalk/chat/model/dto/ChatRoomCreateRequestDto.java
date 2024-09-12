b7298ead44ec9d74283b709f0a8d8e9b1cc585ce
leedabin2 <12193152@inha.edu>
2024-09-12 17:31:06 +0900

    fix: 채팅방 생성시 chatDuration 자유 일때, 찬반 일때 분리

--- src/main/java/server/cubeTalk/chat/model/dto/ChatRoomCreateRequestDto.java
+++ src/main/java/server/cubeTalk/chat/model/dto/ChatRoomCreateRequestDto.java
@@ -16,7 +16,6 @@ public class ChatRoomCreateRequestDto {
     @Size(min = 3, max = 50, message = "제목은 3자 이상 50자 이하이어야 합니다.")
     private String title;
     private String description;
-//    @Min(value = 6, message = "찬반토론은 최소 6명이상부터 가능합니다.")
     private int maxParticipants;
     @NotEmpty(message = "채팅방 모드는 필수 항목입니다.")
     private String chatMode;
