9d148c703488e14c562e4a79caf936085a85e968
leedabin2 <12193152@inha.edu>
2024-09-12 16:44:51 +0900

    fix: participants 엔티티 닉네임 필드 추가

--- src/main/java/server/cubeTalk/chat/model/entity/Participant.java
+++ src/main/java/server/cubeTalk/chat/model/entity/Participant.java
@@ -10,9 +10,10 @@ public class Participant {
     private String memberId;
     private String role;
     private String status;
+    private String nickName;
 
-    public static Participant changeRole(Participant participant, String newRole, String status) {
-        return new Participant(participant.getMemberId(), newRole, participant.getStatus());  // 새 객체 반환
+    public static Participant changeRole(Participant participant, String newRole, String status, String nickName) {
+        return new Participant(participant.getMemberId(), newRole, participant.getStatus(), participant.getNickName());  // 새 객체 반환
     }
 
 }
