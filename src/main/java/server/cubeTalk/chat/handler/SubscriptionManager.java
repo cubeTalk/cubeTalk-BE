2809cb791fc5f0b51084debcc196346d00f2ee20
leedabin2 <12193152@inha.edu>
2024-09-20 03:29:50 +0900

    feat: 구독 세션 관리 닉네임 매핑 추가 (채팅 시작 후 progress channel에 대한 검증 추가)

--- src/main/java/server/cubeTalk/chat/handler/SubscriptionManager.java
+++ src/main/java/server/cubeTalk/chat/handler/SubscriptionManager.java
@@ -3,6 +3,7 @@ package server.cubeTalk.chat.handler;
 import org.springframework.stereotype.Component;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -10,11 +11,23 @@ import java.util.concurrent.ConcurrentHashMap;
 public class SubscriptionManager {
     // 구독 상태를 관리하는 맵 (세션 ID -> 구독 채널 리스트)
     private final ConcurrentHashMap<String, Set<String>> subscriptionMap = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, String> sessionNickNameMap = new ConcurrentHashMap<>();
 
-    // 구독 요청이 들어오면 구독 상태를 저장
+    /* 구독 요청이 들어오면 구독 상태를 저장 */
+    // 닉네임이 있을 때 사용하는 메서드
+    public void addSubscription(String sessionId, String channelId, String nickName) {
+        subscriptionMap.computeIfAbsent(sessionId, k -> new HashSet<>()).add(channelId);
+
+        if (nickName != null && !nickName.isEmpty()) {
+            sessionNickNameMap.put(sessionId, nickName);
+        }
+    }
+
+    // 닉네임 없이 구독만 처리하는 메서드
     public void addSubscription(String sessionId, String channelId) {
         subscriptionMap.computeIfAbsent(sessionId, k -> new HashSet<>()).add(channelId);
     }
+
 
     // 구독 해제 시 구독 상태에서 제거
     public void removeSubscription(String sessionId, String channelId) {
@@ -32,6 +45,7 @@ public class SubscriptionManager {
     // 세션이 끊길 때 해당 세션의 모든 구독을 제거
     public void removeSession(String sessionId) {
         subscriptionMap.remove(sessionId);
+        sessionNickNameMap.remove(sessionId);
     }
 
 
@@ -40,5 +54,23 @@ public class SubscriptionManager {
                 System.out.println("Session: " + sessionId + " -> Channels: " + channels)
         );
     }
+
+    public boolean isNickNameInList(List<String> nickNames) {
+        final boolean[] isNickNameFound = {false};
+
+        subscriptionMap.forEach((sessionId, channels) -> {
+            String nickName = sessionNickNameMap.get(sessionId); // sessionNickNameMap에서 sessionId로 닉네임 가져오기
+            System.out.println("Session: " + sessionId + " -> Channels: " + channels);
+
+            if (nickName != null && !nickName.isEmpty() && nickNames.contains(nickName)) {
+                isNickNameFound[0] = true;
+            }
+        });
+
+        return isNickNameFound[0];
+    }
+
+
+
 }
 
