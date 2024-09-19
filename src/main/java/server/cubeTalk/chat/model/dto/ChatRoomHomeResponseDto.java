a98912c648cfa46fed9a251837c07ed9520e0931
leedabin2 <12193152@inha.edu>
2024-09-20 01:36:07 +0900

    fix: home response maxParticipants, chatDuration 필드 추가

--- src/main/java/server/cubeTalk/chat/model/dto/ChatRoomHomeResponseDto.java
+++ src/main/java/server/cubeTalk/chat/model/dto/ChatRoomHomeResponseDto.java
@@ -13,11 +13,16 @@ import server.cubeTalk.chat.model.entity.DebateSettings;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ChatRoomHomeResponseDto {
     private String description;
+    private Double chatDuration;
+    private int maxParticipants;
     private DebateSettings debateSettings;
+
 
     public static ChatRoomHomeResponseDto fromChatRoom(ChatRoom chatRoom) {
         return ChatRoomHomeResponseDto.builder()
                 .description(chatRoom.getDescription())
+                .chatDuration(chatRoom.getChatDuration())
+                .maxParticipants(chatRoom.getMaxParticipants())
                 .debateSettings(chatRoom.getChatMode().equals("자유") ? null : chatRoom.getDebateSettings())
                 .build();
     }
